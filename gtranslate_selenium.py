9a0421cec3ecc63f4d28716ef410f5bc00d16a40
Dekel Cohen <dekelcohen@gmail.com>
2023-11-06 19:35:17 +0200

    gtranslate_selenium.py

---
+++ gtranslate_selenium.py
@@ -0,0 +1,116 @@
+import os
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pathlib import Path
+import time
+from plyer import notification
+from loguru import logger
+
+# Define button constants
+FILE_INPUT_NAME = "file"
+DOCUMENTS_BUTTON_XPATH = "//button[span[contains(., 'Documents')]]"
+TRANSLATE_BUTTON_XPATH = "//button[span[contains(., 'Translate')]]"
+DOWNLOAD_BUTTON_XPATH = "//button[span[contains(., 'Download translation')]]"
+CLEAR_FILE_BUTTON_CSS_SELECTOR = "button[aria-label='Clear file']"
+
+def show_tooltip_message(message):
+    notification.notify(
+        title="Translation Instructions",
+        message=message,
+        timeout=10
+    )
+
+def google_translate_folder_of_excels(folder_path):
+    """
+
+    Parameters
+    ----------
+    folder_path : Path to folder with .xlsx files to translate
+    ***** Important: <= 1MB per file, as it may refuse to translate several larger files in seq 
+    -------
+    None.
+
+    """
+    options = uc.options.ChromeOptions() #webdriver.ChromeOptions()
+
+    # Set the language to English
+    options.add_argument("--lang=en")
+    options.headless = False
+    
+
+    # Create a new Chrome driver instance
+    browser = uc.Chrome(options) # webdriver.Chrome(options=options)
+    
+    
+    # Display tooltip message using plyer
+    show_tooltip_message("Please select the 'Documents' tab and set source and target languages.")
+    # Wait for 10 seconds - to allow selection of Documents tab | source and target langs
+    time.sleep(20)
+    
+    
+    # Get the Windows Downloads folder path
+    downloads_folder = os.path.expanduser("~/Downloads")
+    
+    # Loop through all .xlsx files in the folder and upload them one by one
+    for file_path in Path(folder_path).glob("*.xlsx"):
+        try:
+            # Check if the file already exists in the Downloads folder, and if so, skip it
+            if os.path.exists(os.path.join(downloads_folder, file_path.name)):
+                logger.info(f"Skipping {file_path.name} as it already exists in the Downloads folder.")
+                continue
+            
+            # Navigate to the desired website
+            browser.get("https://translate.google.com")
+            
+            # Use WebDriverWait to wait for the "Translate" button with a span containing "Translate"
+            translate_button = WebDriverWait(browser, 120).until(
+                EC.element_to_be_clickable((By.XPATH, DOCUMENTS_BUTTON_XPATH))
+            )
+            
+            # Log the file currently being translated
+            logger.info(f"Translating {file_path.name}")
+            
+            file_input = WebDriverWait(browser, 30).until(
+                EC.presence_of_element_located((By.NAME, FILE_INPUT_NAME))
+            )
+            
+            # Log info before uploading the file
+            logger.info(f"Uploading {file_path.name}")
+            file_input.send_keys(str(file_path))
+            
+            # Use WebDriverWait to wait for the "Translate" button with a span containing "Translate"
+            translate_button = WebDriverWait(browser, 120).until(
+                EC.element_to_be_clickable((By.XPATH, TRANSLATE_BUTTON_XPATH))
+            )
+            
+            # Log info before clicking the "Translate" button
+            logger.info("Clicking the 'Translate' button")
+            translate_button.click()
+                        
+            # Now that it's "Translating...", wait to click the "Download" button
+            download_button = WebDriverWait(browser, 120).until(
+                EC.element_to_be_clickable((By.XPATH, DOWNLOAD_BUTTON_XPATH))
+            )
+            
+            # Log info before clicking the "Download" button
+            logger.info("Clicking the 'Download' button")
+            download_button.click()
+            
+            
+            # Log success message
+            logger.info(f"Translation of {file_path.name} completed successfully.")
+        
+        except Exception as e:
+            # Log error and continue to the next file
+            logger.error(f"Error while translating {file_path.name}: {str(e)}")
+            continue
+        
+    time.sleep(120) # wait for last file to download (or result is a partial download file)    
+    browser.close()
+    browser.quit()
+
+# Call the function with the desired folder path
+folder_path = r"D:\NLP\web_scrape\temp\he"
+google_translate_folder_of_excels(folder_path)
--- translate_dataset.py
+++ translate_dataset.py
@@ -1,1 +1,41 @@
-#
+import string
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import unittest
+from loguru import logger
+
+
+def translate_testset():
+     from nlp_utils.translate.translate_ds import TranslateUtil
+     # Create translation - Start with english test execution --> df_gt_prepro.parquet eng is saved
+     # Important: Every change in fix_labels / fix types ... --> need to retranslate
+     folder_path = './temp'
+     target_lang = 'he'
+     def read_df_first_letter_upper_case(path):
+          df = pd.read_parquet(path)
+          df['original_name'] = df['original_name'].apply(lambda x: string.capwords(x))
+          df['matched_name'] = df['matched_name'].apply(lambda x: string.capwords(x))
+          return df
+     file_name = 'df_gt_prepro.parquet'
+     trutil = TranslateUtil('./temp', [file_name], 
+                            tr_cols = ['original_name','matched_name'], 
+                            copy_cols = ['type','exception','match'], 
+                            copy_rename_cols =['original_name','matched_name'], 
+                            read_df_func = read_df_first_letter_upper_case)
+     # 1) Save .xlsx files with eng, ready to upload
+     trutil.save_eng_xls()
+     # 2) Manually upload to Google Translate , the .xlsx files from eng_for_tr folder created in prev step --> save translations to ar
+     # 3) create final files with column names in eng + copied columns from eng .xlsx
+     ar_path = Path(folder_path) / target_lang
+     trutil.process_tr_files(ar_path)
+     df_gt_prepro_ar = pd.read_csv(Path(ar_path) / (Path(file_name).stem +'.csv') ,sep='\t',encoding='utf-8')
+     # Filter out rows that are in english - not translated 
+     RE_ENG_CHARS = r"[a-zA-Z._-]{3,}"
+     df_gt_prepro_ar = df_gt_prepro_ar[~df_gt_prepro_ar.original_name.str.contains(RE_ENG_CHARS, regex = True) & ~df_gt_prepro_ar.matched_name.str.contains(RE_ENG_CHARS, regex = True)]
+     # Filter out rows that orig name == match name (They weren't eq in eng, but after translation they are eq). 
+     # There is neq filter at each run, but also here - allows us to explore the translated pairs 
+     df_gt_prepro_ar = df_gt_prepro_ar[df_gt_prepro_ar.original_name.str.trim().str.lower() != df_gt_prepro_ar.matched_name.trim().str.str.lower()]
+          
+     # Save as .xlsx in the test folder - ready to commit
+     df_gt_prepro_ar.to_excel( Path(GT_PATH) / target_lang / (Path(file_name).stem + '.xlsx') )
