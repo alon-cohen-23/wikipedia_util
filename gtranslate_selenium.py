471430c7e8d18f499ab9b0316d8f16b14d5f5be4
alon-cohen-23 <138993462+alon-cohen-23@users.noreply.github.com>
2023-11-09 00:22:55 +0200

    Update gtranslate_selenium.py

--- gtranslate_selenium.py
+++ gtranslate_selenium.py
@@ -1,6 +1,6 @@
 import os
 import undetected_chromedriver as uc
-from selenium import webdriver
+#from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,29 +16,44 @@ TRANSLATE_BUTTON_XPATH = "//button[span[contains(., 'Translate')]]"
 DOWNLOAD_BUTTON_XPATH = "//button[span[contains(., 'Download translation')]]"
 CLEAR_FILE_BUTTON_CSS_SELECTOR = "button[aria-label='Clear file']"
 
+<<<<<<< HEAD
 
-def google_translate_folder_of_excels(folder_path, chromedriver_path):
+def show_tooltip_message(message):
+    notification.notify(
+        title="Translation Instructions",
+        message=message,
+        timeout=10
+    )
+=======
+>>>>>>> 0f4815a75495b14d8a73a41ae9e3a9b52c12832b
+
+def google_translate_folder_of_excels(folder_path):
     """
 
     Parameters
     ----------
     folder_path : Path to folder with .xlsx files to translate
-    chromedriver_path: Path to the chromr driver 
+    chromedriver_path : Path to the chromr driver 
     ***** Important: <= 1MB per file, as it may refuse to translate several larger files in seq 
     -------
     None.
 
     """
-    options =  webdriver.ChromeOptions() #uc.options.ChromeOptions()
+    options =  uc.options.ChromeOptions() #webdriver.ChromeOptions()
 
     # Set the language to English
     options.add_argument("--lang=en")
     options.headless = False
-    options.add_argument(f'--chromedriver-executable={chromedriver_path}')
+    #options.add_argument(f'--chromedriver-executable={chromedriver_path}')
 
     # Create a new Chrome driver instance
+<<<<<<< HEAD
+    browser =  uc.Chrome(options) #webdriver.Chrome(options=options)
+    # Display tooltip message using plyer
+=======
     browser =  webdriver.Chrome(options=options) #uc.Chrome(options)
     
+>>>>>>> 0f4815a75495b14d8a73a41ae9e3a9b52c12832b
     
     # Get the Windows Downloads folder path
     downloads_folder = os.path.expanduser("~/Downloads")
@@ -52,16 +67,16 @@ def google_translate_folder_of_excels(folder_path, chromedriver_path):
                 continue
             
             # Navigate to the desired website
-            browser.get("https://translate.google.com/?hl=iw&sl=auto&tl=en&op=docs")
+            browser.get("https://translate.google.com/?sl=auto&tl=en&op=docs")
             
-            # Use WebDriverWait to wait for the "Translate" button with a span containing "Translate"
+            """# Use WebDriverWait to wait for the "Translate" button with a span containing "Translate"
             translate_button = WebDriverWait(browser, 120).until(
                 EC.element_to_be_clickable((By.XPATH, DOCUMENTS_BUTTON_XPATH))
             )
             
             # Log the file currently being translated
             logger.info(f"Translating {file_path.name}")
-            
+            """
             file_input = WebDriverWait(browser, 30).until(
                 EC.presence_of_element_located((By.NAME, FILE_INPUT_NAME))
             )
@@ -102,6 +117,6 @@ def google_translate_folder_of_excels(folder_path, chromedriver_path):
     browser.quit()
 
 # Call the function with the desired folder path
-folder_path = r"H:/Alon/wikipedia_util/tr_dataset"
-chromedriver_path = 'H:/Alon/wikipedia_util/chromedriver.exe'
-google_translate_folder_of_excels(folder_path, chromedriver_path)
+folder_path = r"/Users/aloncohen/Documents/wikipedia_util/he_tr_excel"
+chromedriver_path = '/Users/aloncohen/Documents/large_files/chromedriver-mac-arm64/chromedriver'
+google_translate_folder_of_excels(folder_path)
