f573db6728fe34ceb99a4562242cfccbaaee0d7d
urihein <urihei@gmail.com>
2023-11-28 14:17:51 +0200

    adding ner

--- df_process.py
+++ df_process.py
@@ -5,84 +5,86 @@ Created on Thu Nov  2 23:28:54 2023
 @author: family123
 """
 
-import pandas as pd 
+import pandas as pd
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
 from pathlib import Path
 
-def filter_sentences_df (df):
-   # filter the length of the sentences (between 4-30 words)
-   df['word_count'] = df['HE_sentences'].apply(lambda x: count_words(x))
-   len_filtered_df = df.query('word_count > 3 and word_count < 31').copy()
-   
-   #filter the df to only hebrew sentences
-   len_filtered_df['sen_lang'] = len_filtered_df['HE_sentences'].apply(lambda x: detect_lang(x))
-   He_filtered_df = len_filtered_df.query("sen_lang == 'he'")
-   
-   He_filtered_df = He_filtered_df[['title','HE_sentences']]
-   return He_filtered_df
-    
+
+def filter_sentences_df(df):
+    # filter the length of the sentences (between 4-30 words)
+    df['word_count'] = df['HE_sentences'].apply(lambda x: count_words(x))
+    len_filtered_df = df.query('word_count > 3 and word_count < 31').copy()
+
+    # filter the df to only hebrew sentences
+    len_filtered_df['sen_lang'] = len_filtered_df['HE_sentences'].apply(lambda x: detect_lang(x))
+    He_filtered_df = len_filtered_df.query("sen_lang == 'he'")
+
+    He_filtered_df = He_filtered_df[['title', 'HE_sentences']]
+    return He_filtered_df
+
+
 def count_words(cell_content):
     words = cell_content.split()
-    return len(words)    
+    return len(words)
 
-def detect_lang (cell_content):
+
+def detect_lang(cell_content):
     try:
         return detect(cell_content)
     except LangDetectException:
         return 'unknown'
 
-def split_df (df):
-    copied_df = df.copy() 
-    copied_df = copied_df.drop(columns= ['title'])
-    
-    for number in range (int(len(df)/30000+1)): # the max size for google translate 
-        if len(df)>30000:
+
+def split_df(df):
+    copied_df = df.copy()
+    copied_df = copied_df.drop(columns=['title'])
+
+    for number in range(int(len(df) / 30000 + 1)):  # the max size for google translate
+        if len(df) > 30000:
             part_df = copied_df.head(30000)
             copied_df = copied_df.drop(copied_df.index[:30000])
         else:
             part_df = copied_df
-            
-        part_df.to_excel(f'he_tr_excel/{number}_part_HE.xlsx', index=False)    
-        
+
+        part_df.to_excel(f'he_tr_excel/{number}_part_HE.xlsx', index=False)
+
+
 def concat_dir_excels(folder_path):
     concat_df = pd.DataFrame()
-    
+
     for file_path in Path(folder_path).glob("*.xlsx"):
         file_df = pd.read_excel(file_path)
-        concat_df = pd.concat([concat_df, file_df], ignore_index=True)        
-    
+        concat_df = pd.concat([concat_df, file_df], ignore_index=True)
+
     return concat_df
-def create_concatenated_translated_df (he_folder_path,en_folder_path):
+
+
+def create_concatenated_translated_df(he_folder_path, en_folder_path):
     """
     Parameters
     ----------
-    he_folder_path : path to the folder that contains the splited Hebrew sentences df 
-    en_folder : path to the folder that contains the splited translated English sentences df 
+    he_folder_path : path to the folder that contains the splited Hebrew sentences df
+    en_folder : path to the folder that contains the splited translated English sentences df
 
     Returns
     -------
     concatenated df that contains the data of all of the files from both folders, drop the duplicates,
     and has 2 columns: HE_sentences, EN_sentences
-    """        
+    """
     df = concat_dir_excels('tr_data/he_tr_excel')
-    
+
     en_df = concat_dir_excels('tr_data/en_tr_excel')
-    en_df = en_df.rename(columns = {'HE_sentences': 'EN_sentences'})
-    
+    en_df = en_df.rename(columns={'HE_sentences': 'EN_sentences'})
+
     df['EN_sentences'] = en_df['EN_sentences']
-    df = df.drop_duplicates(subset=['HE_sentences']) # I translated the duplicates as well so I cant drop them in the beggining
-    
+    df = df.drop_duplicates(
+        subset=['HE_sentences'])  # I translated the duplicates as well so I cant drop them in the beggining
+
     df = df.reset_index(drop=True)
-    
+
     return df
-    
-if __name__ =='__main__':
-    
-    
-    
-    
-    
-    
-    
-                
+
+
+if __name__ == '__main__':
+    pass
--- parse_dump.py
+++ parse_dump.py
@@ -12,21 +12,22 @@ from nltk.tokenize import sent_tokenize
 import pandas as pd
 from df_process import filter_sentences_df, split_df
 
-
-RE_BOLD = re.compile(r"'''(.*?)'''",re.MULTILINE | re.IGNORECASE)
+RE_BOLD = re.compile(r"'''(.*?)'''", re.MULTILINE | re.IGNORECASE)
 
 
 def process_dump(dump, path):
-  for page in dump:
-      yield page
+    for page in dump:
+        yield page
+
 
 dump_path = r'H:\Alon\large_files/hewiki-latest-pages-articles.xml.bz2'
 paths = [dump_path]
 dump_gen = mwxml.map(process_dump, paths)
 
+
 def extract_rev_first_para(rev_text):
     wikicode = mwparserfromhell.parse(rev_text)
-    #templates = wikicode.filter_templates()
+    # templates = wikicode.filter_templates()
     # First paragraph bolds
     # First section, filtered from templates lines (infobox, about, cite ...)
     sections = wikicode.get_sections()
@@ -35,74 +36,80 @@ def extract_rev_first_para(rev_text):
     tpl_lines = set('\n'.join([str(tpl) for tpl in sec_tpls]).split('\n'))
     text_lines = [line for line in sec_lines if not line in tpl_lines]
     return text_lines
-    
-def extract_page_first_para(page):    
+
+
+def extract_page_first_para(page):
     first_para_lines = None
-    if page.redirect: # Do not process redirect pages for now
+    if page.redirect:  # Do not process redirect pages for now
         return first_para_lines
-    
-    for revision in page: # hopefull only a single revision, as we exported only latest version                
+
+    for revision in page:  # hopefull only a single revision, as we exported only latest version
         first_para_lines = extract_rev_first_para(revision.text)
         break
     return first_para_lines
 
-def extract_first_first_bold_span_from_1st_sent(lines):    
+
+def extract_first_first_bold_span_from_1st_sent(lines):
     bold_spans = []
     if len(lines) > 0:
         matches = RE_BOLD.finditer(lines[0])
-        for matchNum, match in enumerate(matches, start=1):    
-            #print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-            
+        for matchNum, match in enumerate(matches, start=1):
+            # print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+
             for groupNum in range(0, len(match.groups())):
-                groupNum = groupNum + 1            
-                print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+                groupNum = groupNum + 1
+                print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum,
+                                                                                start=match.start(groupNum),
+                                                                                end=match.end(groupNum),
+                                                                                group=match.group(groupNum)))
                 bold_spans.append(match.group(groupNum))
     return bold_spans
 
-        
+
 def main():
     data_frames = []  # Create a list to store DataFrames
-        
+
     for index, page in enumerate(dump_gen):
-       
+
         page_wikicode = extract_page_wikicode(page)
-        page_sentences = extract_sentences_from_wikicode (page_wikicode)
-    
+        page_sentences = extract_sentences_from_wikicode(page_wikicode)
+
         data_frames.append(pd.DataFrame({'title': page.title, 'HE_sentences': page_sentences}))
-                
-        if index >1000:
+
+        if index > 1000:
             break
-    
+
     sentences_df = pd.concat(data_frames, ignore_index=True)
-    
-    return sentences_df    
 
-def extract_sentences_from_wikicode (wiki_text):
-     wikicode = mwparserfromhell.parse(wiki_text)
-     text = str(wikicode.strip_code())
-     
-     lines = text.split('\n')
-     sentences = []
-     for line in lines:
-         sentences.extend(sent_tokenize(line))
-      
-     sentences = [sentence for sentence in sentences if '|' not in sentence]
-     return sentences 
+    return sentences_df
 
-def extract_page_wikicode(page):    
+
+def extract_sentences_from_wikicode(wiki_text):
+    wikicode = mwparserfromhell.parse(wiki_text)
+    text = str(wikicode.strip_code())
+
+    lines = text.split('\n')
+    sentences = []
+    for line in lines:
+        sentences.extend(sent_tokenize(line))
+
+    sentences = [sentence for sentence in sentences if '|' not in sentence]
+    return sentences
+
+
+def extract_page_wikicode(page):
     page_wikicode = None
-    if page.redirect: # Do not process redirect pages for now
+    if page.redirect:  # Do not process redirect pages for now
         return page_wikicode
-    
-    for rev in page: # hopefull only a single revision, as we exported only latest version                
+
+    for rev in page:  # hopefull only a single revision, as we exported only latest version
         page_wikicode = mwparserfromhell.parse(rev.text)
         break
-    return page_wikicode          
+    return page_wikicode
 
-            
+
 if __name__ == '__main__':
-    df = pd.read_parquet('/Users/aloncohen/Documents/large_files/wikipedia_util_tr_data/relevant_categories_sentences.parquet')
+    df = pd.read_parquet(
+        '/Users/aloncohen/Documents/large_files/wikipedia_util_tr_data/relevant_categories_sentences.parquet')
     df = filter_sentences_df(df)
     split_df(df)
-    
-   
