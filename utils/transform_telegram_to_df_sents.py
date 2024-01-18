f6007942726072eae5e34b158f6384452513f8cd
Dekel Cohen <dekelcohen@gmail.com>
2024-01-18 19:46:05 +0200

    nllb-200-distilled-600M_arb_eng_with_telegram_4M Problem: only mean comt 76 - compared to 79 before added the 4M yonatan

--- utils/transform_telegram_to_df_sents.py
+++ utils/transform_telegram_to_df_sents.py
@@ -2,8 +2,18 @@ from pathlib import Path
 import pandas as pd
 from eng_utils.common.common_util import read_df_folder
 
-
+def convert_yonathan_languages_col_format_to_df_sents(data_folder):
+    df = pd.read_parquet(r"D:\NLP\Translation\wikipedia_util\data\raw\telegram\translation_dataset_v1.parquet")
+    df = df[(df.languages == 'ar') | (df.languages == 'ara')]
+    df= df.rename(columns={'en' : 'EN_sentences', 'original_language' : 'HE_sentences'})
+    df = df[['EN_sentences', 'HE_sentences']]
+    df.iloc[10001].EN_sentences
+    df.iloc[10001].HE_sentences
+    df.to_parquet(data_folder / 'translation_yonatan_sents_ar_en.parquet')
+    
 if __name__ == "__main__":
+    
+    
     data_folder = Path(r"D:\NLP\Translation\wikipedia_util\data\raw")
     df = read_df_folder(data_folder)
     df.info()
